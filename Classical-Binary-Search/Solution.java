e6f58273e1438a949cafc9505abd0502b363bd99
qiaochu-caishisan <logan_chu@outlook.com>
2016-11-10 00:02:36 -0500

    Create Solution.java

---
+++ Classical-Binary-Search/Solution.java
@@ -0,0 +1,34 @@
+public class Solution {
+    /**
+     * @param nums: An integer array sorted in ascending order
+     * @param target: An integer
+     * @return an integer
+     */
+    public int findPosition(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return -1;
+        }
+        
+        int start = 0;
+        int end  = nums.length - 1;
+        
+        while(start + 1 < end){
+            int mid = start + (end - start)/2;
+            if(target == nums[mid]){
+                return mid;
+            }else if(target < nums[mid]){
+                end = mid;
+            }else{
+                start = mid;
+            }
+        }
+        
+        if(target == nums[start]){
+            return start;
+        }else if(target == nums[end]){
+            return end;
+        }else{
+            return -1;
+        }
+    }
+}

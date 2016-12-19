b7cb56ad5ee61216484888ab43018b135ac7da47
qiaochu-caishisan <logan_chu@outlook.com>
2016-12-19 16:25:07 -0500

    Create Solution1.java

---
+++ Subarray Sum/Solution1.java
@@ -0,0 +1,24 @@
+public class Solution {
+    /**
+     * @param nums: A list of integers
+     * @return: A list of integers includes the index of the first number 
+     *          and the index of the last number
+     */
+    public ArrayList<Integer> subarraySum(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        map.put(0, -1);
+        int sum = 0;
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        for(int i = 0; i < nums.length; i++){
+            sum += nums[i];
+            if(map.containsKey(sum)){
+                result.add(map.get(sum) + 1);
+                result.add(i);
+                return result;
+            }
+            map.put(sum, i);
+        }
+        return result;
+    }
+}

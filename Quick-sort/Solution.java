15ad2fd98424b512eb5858458008dd8364400f7c
qiaochu-caishisan <logan_chu@outlook.com>
2017-02-25 23:03:29 -0500

    Update Solution.java

--- Quick-sort/Solution.java
+++ Quick-sort/Solution.java
@@ -4,90 +4,50 @@ public class Solution {
      * @return void
      */
      //quick sort
-     public void sortIntegers2(int[] A){
-         if(A == null || A.length == 0){
-             return;
-         }
-         int start = 0;
-         int end = A.length - 1;
-         quickSort(A, start, end);
-     }
-     private void quickSort(int[] A, int start, int end){
-         if(start >= end){
-             return;
-         }
-         int left = start;
-         int right = end;
-         int pivot = A[(start + end)/2];
-         while(left <= right){
-         while(left <= right && A[left] < pivot){
-             left++;
-         }
-         while(left <= right && A[right] > pivot){
-             right--;
-         }
-          if(left <= right){
-             int temp = A[right];
-             A[right] = A[left];
-             A[left] = temp;
-             left++;
-             right--;
-          }
-    }
-         
-         quickSort(A, start, right);
-         quickSort(A, left, end);
-         
-         
-     }
-    
-}
-    
-    /**mergesort
     public void sortIntegers2(int[] A) {
-        int[] temp = new int[A.length];
-        mergeSort(A, 0, A.length - 1, temp);
-    }
-    private void mergeSort(int[] A, int start, int end, int[] temp){
-        if(start >= end){
+        // Write your code here
+        if(A.length == 0 || A == null){
             return;
         }
-        int mid = start + (end - start)/2;
-        mergeSort(A, start, mid, temp);
-        mergeSort(A, mid + 1, end, temp);
-        merge(A, start, mid, end, temp);
+        quicksort(A, 0, A.length - 1);
     }
-    private void merge(int[] A, int start, int mid, int end, int[] temp){
-        int left = start;
-        int right = mid + 1;
-        int index = start;
+    public void quicksort(int[] A, int l, int r){
+        if(l >= r){
+            return;
+        }
         
-        while(left <= mid && right <= end){
-            if(A[left] < A[right]){
-                temp[index] = A[left];
-                index++;
+        int partition_index = partition(A, l, r);
+        
+            quicksort(A, l, partition_index - 1);
+        
+        
+            quicksort(A, partition_index, r);
+        
+        
+    }
+    public int partition(int[] A, int l, int r){
+         Random rand= new Random();
+         int pivot = A[rand.nextInt(r - l + 1) + l];
+        int left = l;
+        int right = r;
+        while(left <= right){ 
+            while( A[left] < pivot){
                 left++;
-            }else{
-                temp[index] = A[right];
-                right++;
-                index++;
             }
-        }
-        
-        while(left <= mid){
-            temp[index] = A[left];
-            index++;
+            while( A[right] > pivot){
+                right--;
+            }
+            if(left <= right){
+            int temp = A[left];
+            A[left] = A[right];
+            A[right] = temp;
             left++;
+            right--;
         }
-        while(right <= end){
-            temp[index] = A[right];
-            index++;
-            right++;
+           
+            
         }
         
-        for(int i = start;  i <= end; i++){
-            A[i] = temp[i];
-        }
+        return left;
     }
-
-*/
+}
